36ed5b4e37c79323140db50f0844aa0e5dcdc6bc
f13mash <mashf13@gmail.com>
2014-06-13 10:13:25 +0530

    an ultra-light just for console logger

---
+++ index.js
@@ -0,0 +1,49 @@
+/**
+ * Created by f13 on 12/6/14.
+ */
+exports = module.exports = init;
+exports.log = log;
+exports.warn = warn;
+exports.error = error;
+function init(tag) {
+    var method = "log";
+    var splts = tag.split(":");
+    if(splts.length > 1 && (splts[splts.length - 1] === "warn" || splts[splts.length - 1] === "error")) {
+        method = splts[splts.length - 1];
+        splts.pop();
+        tag = splts.join(":");
+    }
+    switch (method) {
+        case "log":
+            return log(tag);
+            break;
+        case "warn":
+            return warn(tag);
+            break;
+        case "error":
+            return warn(tag);
+            break;
+        default :
+            return null;
+    }
+}
+function all(tag) {
+    return {
+        log: log(tag),
+        warn: warn(tag),
+        error: error(tag)
+    }
+}
+function log(tag) {
+    return console.log.bind(undefined, formTag(tag));
+}
+function warn(tag) {
+    return console.warn.bind(undefined, formTag(tag));
+}
+function error(tag) {
+    return console.error.bind(undefined, formTag(tag));
+}
+
+function formTag(tag) {
+    return "["+tag.split("/").join("][")+"]";
+}
